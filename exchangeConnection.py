a3980e6e21222c609db07014876871d6aa8dc424
markac-git <m.cederborg@icloud.com>
2020-11-25 00:15:23 +0100

    First hour of work - extract key values from PDF from url - exchange connection established   - search in inbox DONE

--- exchangeConnection.py
+++ exchangeConnection.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import imaplib, email
 
 user = 'virksomhedX@outlook.dk'
@@ -18,3 +19,55 @@ def search(key, value, connection):
 
 
 print(search('FROM', 'no-reply@microsoft.com', connection))
+=======
+import email
+import imaplib
+import os
+
+user = 'virksomhedX@outlook.dk'
+password = 'vamXat-rowsub-2zibqa'
+host = 'Outlook.office365.com'
+attachment_dir = '/Users/markcederborg/PycharmProjects/InvoiceExtractionExchange/AttachmentDirectory/NewInvoices'
+
+
+def connection(address, password, host):
+    # Connect to the server
+    print('Connecting to ' + host + "...")
+    mail = imaplib.IMAP4_SSL(host, 993)
+    mail.login(address, password)
+    return mail
+
+
+def read_inbox():
+    status, data = mail.search(None, '(UNSEEN)')
+    inbox_item_list = data[0].split()
+    if not inbox_item_list:
+        print('No unread emails')
+    for item in inbox_item_list:
+        result2, email_data = mail.fetch(item, '(RFC822)')
+        raw_email = email_data[0][1].decode("utf-8")
+        email_message = email.message_from_string(raw_email)  # converting to object
+        get_invoices(email_message)
+        mail.uid('STORE', item, '+FLAGS', '\\SEEN')
+
+
+def get_invoices(msg):
+    for part in msg.walk():
+        if part.get_content_maintype() == 'multipart':
+            continue  # skipping to next part in msg.walk()
+        if part.get_content_disposition() is None:
+            continue  # skipping to next part in msg.walk()
+        file_name = part.get_filename()
+        if not file_name.endswith('.pdf'):
+            continue
+        if bool(file_name):
+            file_path = os.path.join(attachment_dir, file_name)
+            with open(file_path, 'wb') as f:
+                f.write(part.get_payload(decode=True))
+        print('Invoice collected')
+
+
+mail = connection(user, password, host)  # calling function for establishing connection
+mail.select('Inbox')  # selecting inbox folder
+read_inbox()
+>>>>>>> First hour of work
